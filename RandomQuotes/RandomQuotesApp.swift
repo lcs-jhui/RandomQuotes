97a20bf3bf9308e4a7e584d82d070a6c83afac8b
Justin Hui <jhui@lcs.on.ca>
2023-04-16 19:57:43 -0400

    Initial Commit

---
+++ RandomQuotes/RandomQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuotesApp.swift
+//  RandomQuotes
+//
+//  Created by Justin Hui on 16/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
