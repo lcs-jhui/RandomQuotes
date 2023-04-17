809bab02f390b67bfc553a3823483550b8b3dda8
Justin Hui <jhui@lcs.on.ca>
2023-04-16 20:55:49 -0400

    Deleted content view and added a quote view in a view group

--- RandomQuotes/RandomQuotesApp.swift
+++ RandomQuotes/RandomQuotesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RandomQuotesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuoteView()
         }
     }
 }
---
+++ RandomQuotes/Views/QuoteView.swift
@@ -0,0 +1,20 @@
+//
+//  QuoteView.swift
+//  RandomQuotes
+//
+//  Created by Justin Hui on 16/4/2023.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuoteView()
+    }
+}
