58bf556715261fe1d7bd2e6ba939d53b70bd3d2c
Justin Hui <jhui@lcs.on.ca>
2023-04-17 22:53:19 -0400

    Added a model to match the structure of the JSON object received from our endpoint.

---
+++ RandomQuotes/Model/Quote.swift
@@ -0,0 +1,18 @@
+//
+//  Quote.swift
+//  RandomQuotes
+//
+//  Created by Justin Hui on 17/4/2023.
+//
+
+import Foundation
+
+struct Quote {
+    let quoteText: String
+    let quoteAuthor: String
+    let senderName: String
+    let senderLink: String
+    let quoteLink: String
+}
+
+let exampleQuote = Quote(quoteText: "We know from science that nothing in the universe exists as an isolated or independent entity.", quoteAuthor: "Margaret Wheatley", senderName: "", senderLink: "", quoteLink: "http://forismatic.com/en/76d16e15e8/")
