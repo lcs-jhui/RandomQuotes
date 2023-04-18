ee95d29b82cbc5da96fd0b81e3a39cdff836ce28
Justin Hui <jhui@lcs.on.ca>
2023-04-18 19:08:14 -0400

    Added a network service helper to load a joke from an endpoint, and displayed it in the user interface.

--- RandomQuotes/Model/Quote.swift
+++ RandomQuotes/Model/Quote.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
-struct Quote {
+struct Quote: Codable {
     let quoteText: String
     let quoteAuthor: String
     let senderName: String
     let senderLink: String
     let quoteLink: String
 }
+
+
 
 let exampleQuote = Quote(quoteText: "We know from science that nothing in the universe exists as an isolated or independent entity.", quoteAuthor: "Margaret Wheatley", senderName: "", senderLink: "", quoteLink: "http://forismatic.com/en/76d16e15e8/")
--- RandomQuotes/Views/QuoteView.swift
+++ RandomQuotes/Views/QuoteView.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 
 struct QuoteView: View {
+    
+    //MARK: Stored Properties
+    @State var currentQuote: Quote?
+    
     var body: some View {
         NavigationView{
             
             VStack{
                 
-                Text("Do or do not, there is no try")
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                Text("-Yoda")
-                    .font(.title2)
-                    .italic()
+                if let currentQuote = currentQuote {
+                    
+                    Text(currentQuote.quoteText)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    Text("-\(currentQuote.quoteAuthor)")
+                        .font(.title2)
+                        .italic()
+                    
+                } else {
+                    //Show a spinning wheel indicator until the joke is loaded
+                    ProgressView()
+                }
+            }
+            .task {
+                currentQuote = await NetworkService.fetch()
             }
             .navigationTitle("Random Quote")
         }
